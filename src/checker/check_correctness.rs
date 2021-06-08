4d09c371cb8201b3af2ea616e01af196e69465c1
sorkierkegaard <es.luismiguelbaez@gmail.com>
2021-06-07 20:25:04 -0500

    feat: added the comparer that checks if a test case has the wrong answer

--- src/checker/check_correctness.rs
+++ src/checker/check_correctness.rs
@@ -4,6 +4,7 @@
  *  License: MIT (See the LICENSE file in the repository root directory)
  */
 
+use std::collections::VecDeque;
 use std::path::Path;
 use std::path::PathBuf;
 use std::fs;
@@ -97,7 +98,10 @@ pub fn run(target_file: PathBuf, correct_file: PathBuf,
 
     generator_file_cpp.compile();
 
+    // TODO: if wa is true, remove testcase_tle_*.txt
+
     let mut tle_count: u32 = 0;
+    let mut wa_count: u32 = 0;
 
     for test_number in 1..=test_cases {
         let time_gen: Duration = generator_file_cpp.execute(timeout as u32);
@@ -108,7 +112,6 @@ pub fn run(target_file: PathBuf, correct_file: PathBuf,
 
         if time_gen >= Duration::from_millis(timeout as u64) {
             // TLE Generator
-
             println!(
                 "  {} [{}] {} {}ms",
                 test_number,
@@ -123,7 +126,6 @@ pub fn run(target_file: PathBuf, correct_file: PathBuf,
 
         if time_correct >= Duration::from_millis(timeout as u64) {
             // TLE Correct file
-
             println!(
                 "  {} [{}] {} {}ms",
                 test_number,
@@ -149,12 +151,12 @@ pub fn run(target_file: PathBuf, correct_file: PathBuf,
                 timeout
             );
         
-            // Verify that the folder wa_cases exists, in case it does not exist create it
-            if save_cases && !Path::new("wa_cases").exists() {
-                match fs::create_dir("wa_cases") {
+            // Verify that the folder test_cases exists, in case it does not exist create it
+            if save_cases && !Path::new("test_cases").exists() {
+                match fs::create_dir("test_cases") {
                     Err(_) => {
-                        let error = Err(failure::err_msg("Could not create folder wa_cases"));
-                        return Ok(error.context("wa_cases folder".to_string())?);
+                        let error = Err(failure::err_msg("Could not create folder test_cases"));
+                        return Ok(error.context("test_cases folder".to_string())?);
                     }
                     _ => (),
                 }
@@ -162,9 +164,9 @@ pub fn run(target_file: PathBuf, correct_file: PathBuf,
 
             // Save the input of the test case that gave status tle
             if save_cases {
-                let filename: String = format!("wa_cases/testcase{}.txt", tle_count);
+                let filename: String = format!("test_cases/testcase_tle_{}.txt", tle_count);
                 let mut file = fs::File::create(filename)
-                    .expect("Error creating file wa_cases/testcase(n).txt");
+                    .expect("Error creating file test_cases/testcase(i).txt");
                 file.write_all(fs::read_to_string("quicktest_input.txt").unwrap().as_bytes()).unwrap();
             }
             
@@ -181,13 +183,51 @@ pub fn run(target_file: PathBuf, correct_file: PathBuf,
                 return Ok(());
             }
         } else {
-            // TODO: Verify WA status
-            println!(
-                "  {} [{}] {} {}ms",
-                test_number.to_string().bold().white(),
-                "OK".bold().green(),
-                "Finished in".bold().green(), mills_target
-            );
+            // The time is in the allowed range
+
+            // Check WA Status
+            if compare_file(&format!("{}/quicktest_output.txt", root), &format!("{}/expected_testcase.txt", root), true) {
+                // is OK
+                println!(
+                    "  {} [{}] {} {}ms",
+                    test_number.to_string().bold().white(),
+                    "OK".bold().green(),
+                    "Finished in".bold().green(), mills_target
+                );
+            } else {
+                // WA found
+                wa_count += 1;
+                println!(
+                    "  {} [{}] {} {}ms",
+                    test_number.to_string().bold().white(),
+                    "WA".bold().red(),
+                    "Finished in".bold().red(), mills_target
+                );
+
+                // Verify that the folder test_cases exists, in case it does not exist create it
+                if save_cases && !Path::new("test_cases").exists() {
+                    match fs::create_dir("test_cases") {
+                        Err(_) => {
+                            let error = Err(failure::err_msg("Could not create folder test_cases"));
+                            return Ok(error.context("test_cases folder".to_string())?);
+                        }
+                        _ => (),
+                    }
+                }
+
+                // Save the input of the test case that gave status tle
+                if save_cases {
+                    let filename: String = format!("test_cases/testcase_wa_{}.txt", wa_count);
+                    let mut file = fs::File::create(filename)
+                        .expect("Error creating file test_cases/testcase(i).txt");
+                    file.write_all(fs::read_to_string("quicktest_input.txt").unwrap().as_bytes()).unwrap();
+                }
+
+                if wa_break {
+                    let error = Err(failure::err_msg(format!("Wrong answer on test {}", test_number)));
+                    return Ok(error.context("WA Status".to_string())?);
+                }
+            }
         }
 
         // remove input, output and error files
@@ -203,4 +243,67 @@ pub fn run(target_file: PathBuf, correct_file: PathBuf,
     fs::remove_file(&"correct.o")?;
 
     Ok(())
+}
+
+pub fn compare_file(target_file: &String, correct_file: &String, ignore_space: bool) -> bool {
+    let mut is_good = true;
+    let target_content = match std::fs::read_to_string(target_file) {
+        Ok(content) => Some(content),
+        Err(_) => {
+            is_good = false;
+            None
+        },
+    };
+
+    let correct_content = match std::fs::read_to_string(correct_file) {
+        Ok(content) => Some(content),
+        Err(_) => {
+            is_good = false;
+            None
+        },
+    };
+
+    if !is_good { return false; }
+
+    let mut target_content_vec: VecDeque<char> = VecDeque::new();
+    let mut correct_content_vec: VecDeque<char> = VecDeque::new();
+    
+    is_good = match (target_content, correct_content) {
+        (Some(_), None) => false,
+        (None, Some(_)) => false,
+        (None, None) => false,
+        (Some(s1), Some(s2)) => {
+            target_content_vec = s1.chars().collect::<VecDeque<char>>();
+            correct_content_vec = s2.chars().collect::<VecDeque<char>>();
+            true
+        },
+    };
+
+    if !is_good { return false; }
+
+    if ignore_space {
+        // Remove spaces at the beginning and end of the file
+        // for target_content_vec
+
+        // TODO: check when the whole vector has blanks, because a Run Time Error is generated
+        while !target_content_vec.is_empty()
+            && *target_content_vec.back().unwrap()==' '||*target_content_vec.back().unwrap()=='\n' {
+            target_content_vec.pop_back();
+        }
+        while !target_content_vec.is_empty()
+            && *target_content_vec.front().unwrap()==' '||*target_content_vec.front().unwrap()=='\n' {
+            target_content_vec.pop_front();
+        }
+        // for correct_content_vec
+        while !correct_content_vec.is_empty()
+            && *correct_content_vec.back().unwrap()==' '||*correct_content_vec.back().unwrap()=='\n' {
+            correct_content_vec.pop_back();
+        }
+        while !correct_content_vec.is_empty()
+            && *correct_content_vec.front().unwrap()==' '||*correct_content_vec.front().unwrap()=='\n' {
+            correct_content_vec.pop_front();
+        }
+    }
+
+    target_content_vec == correct_content_vec
 }
