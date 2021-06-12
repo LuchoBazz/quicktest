1a46ae5bd5e73a4724763030a2c7c61efad10066
sorkierkegaard <es.luismiguelbaez@gmail.com>
2021-06-12 18:01:59 -0500

    test: test for tle subcommand was added

--- tests/cli/tle_subcommand.rs
+++ tests/cli/tle_subcommand.rs
@@ -4,41 +4,98 @@
  *  License: MIT (See the LICENSE file in the repository root directory)
  */
 
-// reference: https://mattgathu.github.io/2017/10/01/testing-rust-cli-apps.html
+// reference I : https://mattgathu.github.io/2017/10/01/testing-rust-cli-apps.html
+// reference II: https://www.duskborn.com/posts/rust-lit/
 
-static WITHOUT_ARGS_OUTPUT: &'static str =
-r#"quicktest-tle 0.3.1
-Check TLE
+static GEN_CPP: &'static str = r#"
+#include <bits/stdc++.h>
+using namespace std;
+template <typename T>
+T random(const T from, const T to) {
+    static random_device rdev;
+    static default_random_engine re(rdev());
 
-USAGE:
-    quicktest tle [FLAGS] [OPTIONS] --gen-file <gen-file> --target-file <target-file>
+    using dist_type = typename conditional<
+        is_floating_point<T>::value,
+        uniform_real_distribution<T>,
+        uniform_int_distribution<T>
+    >::type;
 
-FLAGS:
-    -h, --help          Prints help information
-    -s, --save-cases    Save test cases
-    -b, --tle-break     Break if Time Limit Exceeded (TLE) occurs
-    -V, --version       Prints version information
-
-OPTIONS:
-    -g, --gen-file <gen-file>          Generator File
-    -t, --target-file <target-file>    Target File
-    -n, --test-cases <test-cases>      Number of test cases [default: 1000]
-    -o, --timeout <timeout>            Timeout TLE [default: 2000]
+    dist_type uni(from, to);
+    return static_cast<T>(uni(re));
+}
+int main() {
+    #define endl '\n'
+    int n = random<int>(1e5, 2e5);
+    cout << n << endl;
+    for(int i=0;i<n;++i) cout << random<int>(1, 1e9) << " ";
+    cout << endl;
+    return 0;
+}
 "#;
 
-mod tle_subcommand {
-    use std::process::Command;
-    use crate::cli::tle_subcommand::WITHOUT_ARGS_OUTPUT;
+static TARGET_FILE: &'static str = r#"
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+vector<int> A;
+int main() {
+    cin >> n;
+    A.resize(n);
+    for(auto &ref: A) cin >> ref;
+    sort(A.begin(), A.end());
+    bool change = (A[0] % 1000 == 0);
+    if(change) swap(A[0], A[1]);
+    cout << n << endl;
+    for(auto &a: A) cout << a << " ";
+    cout << endl;
+    return 0;
+}
+"#;
 
-    #[cfg(unix)]
+mod tle_gen_cpp_target_cpp {
+
+    use std::{io::Write, path::PathBuf, process::Command};  // Run programs
+    use assert_cmd::prelude::*; // Add methods on commands
+    use predicates::prelude::*; // Used for writing assertion
+    use std::error::Error;
+
+    fn create_files() -> Result<(), std::io::Error>{
+        use crate::cli::tle_subcommand::GEN_CPP;
+        use crate::cli::tle_subcommand::TARGET_FILE;
+        match std::fs::create_dir("target/code/") {_=>(),}
+        let mut gen_file = std::fs::File::create(PathBuf::from("target/code/gen.cpp"))?;
+        gen_file.write_all(GEN_CPP.as_bytes())?;
+        let mut gen_file = std::fs::File::create(PathBuf::from("target/code/main.cpp"))?;
+        gen_file.write_all(TARGET_FILE.as_bytes())?;
+        Ok(())
+    }
+
+    
     #[test]
-    fn help() {
-        let output = Command::new("./target/debug/quicktest")
-            .arg("tle")
-            .arg("--help")
-            .output()
-            .expect("help in tle subcommand failed");
+    fn run() -> Result<(), Box<dyn Error>> {
+        create_files()?;
+
+        #[cfg(unix)]
+        let mut cmd = Command::new("./target/debug/quicktest");
+
+        #[cfg(windows)]
+        let mut cmd = Command::new("./target/debug/quicktest.exe");
+
+        let cases: usize = 10;
+
+        cmd.arg("tle")
+            .arg("--target-file")
+            .arg("target/code/main.cpp")
+            .arg("--gen-file")
+            .arg("target/code/gen.cpp")
+            .arg("--timeout=1000")
+            .arg(format!("--test-cases={}", cases));
+
+        cmd.assert()
+            .success()
+            .stdout(predicate::str::contains("[OK]").count(cases));
         
-        assert_eq!(String::from_utf8_lossy(&output.stdout), WITHOUT_ARGS_OUTPUT);
+        Ok(())
     }
 }
