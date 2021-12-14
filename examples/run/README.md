272807eb19284e1e6fb14421a0726cccce457d1a
sorkierkegaard <es.luismiguelbaez@gmail.com>
2021-12-14 17:53:52 -0500

    docs: examples for run command was added

---
+++ examples/run/README.md
@@ -0,0 +1,22 @@
+`quicktest run | qt run`
+============
+
+## Run Examples
+
+```shell
+git clone https://github.com/LuisMBaezCo/quicktest.git
+
+cd quicktest/examples/run
+```
+
+* ### cpp examples
+   
+    ```shell
+    quicktest run --target-file=cpp/main.cpp --prefix=test_cases/testcase_ac
+    ```
+
+* ### python examples
+    
+    ```shell
+    quicktest run --target-file=python/main.py --prefix=test_cases/testcase_ac
+    ```
---
+++ examples/run/cpp/main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Maximum Subarray Problem
+    // 1 <= n <= 1e5
+    // -1e5 <= a_i <= 1e5
+    
+    int n; cin >> n;
+    vector<int> values(n);
+    for(int &a: values)
+        cin >> a;
+    
+    // O(n^2)
+    int best = 0;
+    for (int i = 0; i < n; i++) {
+        int sum = 0;
+        for (int j = i; j < n; j++) {
+            sum += values[j];
+            best = max(best, sum);
+        }
+    }
+    cout << best << "\n";
+    return 0;
+}
