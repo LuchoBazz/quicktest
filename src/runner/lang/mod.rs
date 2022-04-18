13aa49782ecd3ff3408457708388373ef58e2cdd
Luis Miguel Báez Aponte <es.luismiguelbaez@gmail.com>
2022-04-18 09:48:07 -0500

    refactory: old cpp and python structures were removed

--- src/runner/lang/mod.rs
+++
@@ -1,8 +0,0 @@
-/*
- *  Quick Test: CLI for stress testing in competitive programming
- *  Copyright (C) 2021 - Luis Miguel Báez
- *  License: MIT (See the LICENSE file in the repository root directory)
- */
-
-pub mod cpp;
-pub mod python;
--- src/runner/mod.rs
+++ src/runner/mod.rs
@@ -5,5 +5,4 @@
  */
 
 pub mod cmd;
-pub mod lang;
 pub mod types;
