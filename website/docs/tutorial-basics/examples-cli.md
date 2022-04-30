3c240b96c9dfe49d1f9c0054fb6f3eb0c4151e11
Luis Miguel Báez Aponte <es.luismiguelbaez@gmail.com>
2022-04-30 17:43:26 -0500

    feat: documentation website using docusaurus was initialized

---
+++ website/docs/tutorial-basics/examples-cli.md
@@ -0,0 +1,7 @@
+---
+sidebar_position: 1
+title: Examples CLI - quicktest
+sidebar_label: Example Quicktest CLI
+---
+
+...
---
+++ website/docusaurus.config.js
@@ -0,0 +1,118 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Quick Test CLI',
+  tagline: 'CLI for stress testing in competitive programming contest',
+  url: 'https://luismbaezco.github.com/quicktest',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'Quick Test CLI', // Usually your GitHub org/user name.
+  projectName: 'quicktest', // Usually your repo name.
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Quick Test CLI',
+        logo: {
+          alt: 'Quick Test CLI Logo',
+          src: 'https://raw.githubusercontent.com/LuisMBaezCo/quicktest/main/assets/logo/quicktest-512x512.png',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Docs',
+          },
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://luismbaezco.github.com/quicktest',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Documentation',
+            items: [
+              {
+                label: 'Docs',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/quicktest',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/quicktest',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/quicktest',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/LuisMBaezCo/quicktest',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © 2021-${new Date().getFullYear()} Quick Test CLI, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
