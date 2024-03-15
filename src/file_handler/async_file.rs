46c29ca21c8b9d66c9f8815c6832333f6aea86fc
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2024-03-15 17:43:57 -0500

    refactor: rename async function 2/2

--- src/file_handler/async_file.rs
+++ src/file_handler/async_file.rs
@@ -1,13 +1,13 @@
 use futures::future::join_all;
 use tokio::{fs::remove_file, io};
 
-pub async fn file_exists(file_name: &str) -> Result<bool, io::Error> {
+pub async fn file_exists_async(file_name: &str) -> Result<bool, io::Error> {
     tokio::fs::File::open(file_name).await?;
     Ok(true)
 }
 
 async fn remove_file_async(file: &str) -> Result<(), io::Error> {
-    if file_exists(file).await? {
+    if file_exists_async(file).await? {
         remove_file(file).await
     } else {
         Ok(())
